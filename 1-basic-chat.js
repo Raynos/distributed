3b536c1c85d9c5bec56b88d5550cbf51f875ad4f
Raynos <raynos2@gmail.com>
2013-06-22 17:22:12 -0700

    first version

---
+++ 1-basic-chat.js
@@ -0,0 +1,60 @@
+var MessageStream = require("message-stream")
+var net = require("net")
+var argv = require("optimist").argv
+var EventEmitter = require("events").EventEmitter
+
+var port = argv.port || 2503
+var id = require("my-local-ip")() + ":" + port
+
+var name = argv.name || "Anonymous"
+
+function Messages() {
+    var messages = new EventEmitter()
+    messages.send = function (text) {
+        messages.emit("message", {
+            text: text,
+            name: name,
+            id: id,
+            time: Date.now()
+        })
+    }
+    messages.receive = function (message, source) {
+        console.log(message.name + " > " + message.text)
+
+        messages.emit("message", message, source)
+    }
+    messages.createStream = function () {
+        var stream = MessageStream(function (message) {
+            messages.receive(message, stream)
+        })
+
+        messages.on("message", function (message, source) {
+            if (source !== stream) {
+                stream.queue(message)
+            }
+        })
+
+        return stream
+    }
+
+    return messages
+}
+
+var messages = Messages()
+process.stdin.on("data", function (text) {
+    messages.send(String(text))
+})
+
+if (argv.server) {
+    var server = net.createServer(function (stream) {
+        stream.pipe(messages.createStream()).pipe(stream)
+    })
+
+    server.listen(port)
+    console.log("Started server on port", port)
+} else {
+    var client = net.connect(port)
+
+    client.pipe(messages.createStream()).pipe(client)
+    console.log("Connected to server on port", port)
+}
